f1710663c751156e18dba2d9c29608ec8ce2cbcd
devsav <savio85@gmail.com>
2018-04-10 21:33:12 -0700

    Implement a stack

---
+++ Stack.py
@@ -0,0 +1,34 @@
+"""
+Stack
+"""
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[len(self.items) -1]
+
+s = Stack()
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
